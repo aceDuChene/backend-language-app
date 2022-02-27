f1e1ab6774277ab9d94986e114b556570931a60f
Velavee <59717974+Velavee@users.noreply.github.com>
2022-02-27 15:38:54 -0800

    fix: run formatter and revert back to original port and host

--- LanguageAppBackend/main.py
+++ LanguageAppBackend/main.py
@@ -33,20 +33,34 @@ def process_audio(language_code):
     else:
         return json.dumps({"text": "Audio Not detected. Try Again."}), 400
 
+
 @app.route("/text-comparison", methods=["POST"])
 def compare_text():
     req = request.json
 
     if "user_answer" not in req or "correct_answer" not in req:
-        return json.dumps({"error": "correct_answer and user_answer must be defined and must be strings"}), status.HTTP_400_BAD_REQUEST
+        return (
+            json.dumps(
+                {
+                    "error": "correct_answer and user_answer must be defined and must be strings"
+                }
+            ),
+            status.HTTP_400_BAD_REQUEST,
+        )
 
-    if isinstance(req["user_answer"], str) is False or isinstance(req["correct_answer"], str) is False:
-        return json.dumps({"error": "correct_answer and user_answer must be strings"}), status.HTTP_400_BAD_REQUEST
-    
+    if (
+        isinstance(req["user_answer"], str) is False
+        or isinstance(req["correct_answer"], str) is False
+    ):
+        return (
+            json.dumps({"error": "correct_answer and user_answer must be strings"}),
+            status.HTTP_400_BAD_REQUEST,
+        )
+
     comparison = TextComparison(req["user_answer"], req["correct_answer"])
     answer = comparison.check_answer()
-    return json.dumps({"answer": answer }), status.HTTP_200_OK
+    return json.dumps({"answer": answer}), status.HTTP_200_OK
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8090, debug=True)
+    app.run(host="'127.0.0.1'", port=8080, debug=True)
--- LanguageAppBackend/text_comparison.py
+++ LanguageAppBackend/text_comparison.py
@@ -2,13 +2,14 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 from pyxdameraulevenshtein import damerau_levenshtein_distance
 
+
 class TextComparison:
     def __init__(self, user_answer, correct_answer):
         self.user_answer = self.remove_puncuation(user_answer)
         self.correct_answer = self.remove_puncuation(correct_answer)
 
     def remove_puncuation(self, sentence):
-        puncuation = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        puncuation = """!()-[]{};:'"\,<>./?@#$%^&*_~"""
         for char in puncuation:
             sentence = sentence.replace(char, "")
 
@@ -23,10 +24,12 @@ class TextComparison:
         # Transpositions are counted as two edits in Levenshtein Distance. Use Damerau-Levenshtein
         # distance to check if difference is a single transposition
         if score >= 96:
-            transposition_score = damerau_levenshtein_distance(self.correct_answer, self.user_answer)
+            transposition_score = damerau_levenshtein_distance(
+                self.correct_answer, self.user_answer
+            )
             if transposition_score <= 1:
                 return True
-    
+
         # Check if string contains the same words in different orders
         any_order_score = fuzz.token_sort_ratio(self.correct_answer, self.user_answer)
         if any_order_score >= 99:
